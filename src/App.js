ee16eb15197a793a99996b69ac7402e366ab014b
uurde <m.ugurdegirmenci@gmail.com>
2023-10-07 13:26:13 +0300

    test commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Uğur
+          Uğur test
         </p>
         <a
           className="App-link"
